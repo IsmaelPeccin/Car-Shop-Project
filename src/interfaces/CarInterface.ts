faea618906046beb2af845fbb03d1678b6ffd068
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-25 19:32:42 -0300

    refactor: Refatorando interfaces para usar os erros da biblioteca zod

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -2,8 +2,15 @@ import { z } from 'zod';
 import { VehicleSchema } from './VehicleInterface';
 
 const CarSchema = z.object({
-  doorsQty: z.number().gte(2).lte(4),
-  seatsQty: z.number().gte(2).lte(7),
+  doorsQty: z.number({
+    required_error: 'Doors quantity is required',
+  }).gte(2, { message: 'DoorsQty must be greater or equal 2' })
+    .lte(4, { message: 'DoorsQty must be less or equal 4' }),
+
+  seatsQty: z.number({
+    required_error: 'Seats quantity is required',
+  }).gte(2, { message: 'Seats quantity must be greater or equal 2' })
+    .lte(7, { message: 'Seats quantity must be less or equal 7' }),
 });
 
 type Car = z.infer<typeof VehicleSchema & typeof CarSchema>;
--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,11 +1,23 @@
 import { z } from 'zod';
 
 const VehicleSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gt(1900).lte(2022),
-  color: z.string().min(3),
+  model: z.string({
+    required_error: 'Model is required', 
+  }).min(3, { message: 'Model must be at least 3 characters' }),
+
+  year: z.number({
+    required_error: 'Year is required',
+  }).gt(1900).lte(2022),
+  
+  color: z.string({
+    required_error: 'Color is required',
+  }).min(3, { message: 'Color must be at least 3 characters' }),
+
   status: z.boolean().optional(),
-  buyValue: z.number().int(),
+
+  buyValue: z.number({
+    required_error: 'Buy value is required',
+  }).int(),
 });
 
 type Vehicle = z.infer<typeof VehicleSchema>;

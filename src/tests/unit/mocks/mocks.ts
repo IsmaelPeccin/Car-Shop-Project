2d07f925fed83bf491293deb9006cfb47d4800e2
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-27 19:44:35 -0300

    test: Mocks para testes

---
+++ src/tests/unit/mocks/mocks.ts
@@ -0,0 +1,17 @@
+export const validCar = {
+  model: 'Fusca',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const invalidCar = {
+  model: 'Fusca',
+  year: 1800,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 1,
+  doorsQty: 10
+};

b8991bbb42fed864e38bac9cb9797d2b0d843707
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-27 19:45:34 -0300

    test: Testes para a camada de Service

---
+++ src/tests/unit/services/service.test.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../service/CarService';
+import { invalidCar, validCar } from '../mocks/mocks';
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+
+describe ('Testing Car Service', () => {
+  before (() => {
+    sinon.stub(carModel, 'create').resolves(validCar);
+  });
+
+  after (() => {
+    (carModel.create as sinon.SinonStub).restore();
+  });
+
+  it ('should create a new car and return new car', async () => {
+    const car = await carService.create(validCar);
+    expect(car).to.be.an('object');
+    expect(car).to.be.equal(validCar);
+  });
+
+  it ('should returns an error when receiving an object without some mandatory properties', async () => {
+    const car = await carService.create(invalidCar);
+    expect(car).to.be.an('object');
+    expect(car).to.be.property('error');
+  });
+});
+

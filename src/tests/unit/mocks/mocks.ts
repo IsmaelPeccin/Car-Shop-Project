5e3b88b1183955d3e4a32807dea8924c21441b4f
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-30 14:53:27 -0300

    test: Add mock para lista de carros com id

--- src/tests/unit/mocks/mocks.ts
+++ src/tests/unit/mocks/mocks.ts
@@ -15,3 +15,42 @@ export const invalidCar = {
   seatsQty: 1,
   doorsQty: 10
 };
+
+export const validCarWithId = [{
+  _id: '5e9f8f8f9c8f8f8f8f8f8f8f',
+  model: 'Fusca',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+},
+{
+  _id: '6e9f8f8f9c8f8f8f8f8f8f8f',
+  model: 'Corcel',
+  year: 1970,
+  color: 'blue',
+  buyValue: 4000,
+  seatsQty: 2,
+  doorsQty: 4
+},
+{
+  _id: '7e9f8f8f9c8f8f8f8f8f8f8f',
+  model: 'Celta',
+  year: 2004,
+  color: 'black',
+  buyValue: 15000,
+  seatsQty: 4,
+  doorsQty: 4
+},
+{
+  _id: '8e9f8f8f9c8f8f8f8f8f8f8f',
+  model: 'Prisma',
+  year: 2010,
+  color: 'white',
+  buyValue: 20000,
+  seatsQty: 4,
+  doorsQty: 4
+}];
+
+
